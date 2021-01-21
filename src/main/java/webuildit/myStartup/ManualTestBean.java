aa30a5279bdbece13d02e44aea6926fa3ee95694
laRose00 <rosenkranzlarissa@gmail.com>
2021-01-21 12:12:35 +0100

    Grundaufbau Projekt

---
+++ src/main/java/webuildit/myStartup/ManualTestBean.java
@@ -0,0 +1,4 @@
+package webuildit.myStartup;
+
+public class ManualTestBean {
+}
---
+++ src/main/java/webuildit/myStartup/SpringDataApplication.java
@@ -0,0 +1,9 @@
+package webuildit.myStartup;
+
+import org.springframework.boot.SpringApplication;
+
+    public class SpringDataApplication {
+         public static void main(String[] args) {
+                SpringApplication.run(webuildit.myStartup.SpringDataApplication.class, args);
+         }
+}
